44c7285c957ff84a7a892452a0ad49822a48daf4
Lxkas <alohasunshineday@yahoo.com>
2024-01-02 22:20:31 +0700

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+const BTC_INFO_URL = "https://api.coinranking.com/v2/coins?search=bitcoin";
+
 function App() {
-	return <div className="text-red-400">Hello world!</div>;
+	const [btcInfo, setBtcInfo] = useState(null);
+
+	useEffect(() => {
+		fetch(BTC_INFO_URL)
+			.then((res) => res.json())
+			.then((data) => setBtcInfo(data))
+			.catch((err) => console.log(err));
+	}, []);
+
+	const { status, data } = btcInfo ? btcInfo : { status: "loading", data: null };
+
+	// BTC UUID Qwsogvtv82FCd
+	return (
+		<div>
+			{status === "success" ? (
+				data.coins.map((coin) => {
+					return (
+						<div key={"coindiv_" + coin.uuid}>
+							<h1>
+								{coin.name} ({coin.symbol}) - {Number(coin.price).toFixed(2)}$
+							</h1>
+						</div>
+					);
+				})
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
 }
 
 export default App;
