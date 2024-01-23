57625d340bf2c1c69c06bd13a0d2eac18780d78d
NayHtetKyaw <58430236+NayHtetKyaw@users.noreply.github.com>
2024-01-23 12:46:37 +0700

    Update src/components/ApplicationHeader.jsx
    Co-authored-by: Tawan <alohasunshineday@yahoo.com>

--- src/components/ApplicationHeader.jsx
+++ src/components/ApplicationHeader.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function ApplicationHeader() {
 	return (
-		<Container className="mb-4 uppercase bg-gray-800/50 p-1">
-			<Link to="/" className="hover:text-orange-600/75">Home</Link>
+		<Container className="mb-4 uppercase">
+			<Link to="/" className="hover:font-bold">
+				Home
+			</Link>
 		</Container>
 	);
 }
